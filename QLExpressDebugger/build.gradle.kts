3490d23a481fdfbdd14a138e6fcb018682bd82da
wanglikang <1024196018@qq.com>
2024-09-05 20:19:21 +0800

    升级支持的idea版本到最新

--- QLExpressDebugger/build.gradle.kts
+++ QLExpressDebugger/build.gradle.kts
@@ -1,13 +1,12 @@
-
 plugins {
     // https://plugins.gradle.org/ 可以子这里找到合适的插件内容
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "2.0.20-Beta2"
+    id("org.jetbrains.kotlin.jvm") version "1.9.24"
     id("org.jetbrains.intellij") version "1.17.3"
 }
 
 group = "com.wlk.ideaPlugin"
-version = "1.1.2"
+version = "1.1.3"
 
 repositories {
     //优先使用本地的maven仓库地址
@@ -20,6 +19,9 @@ repositories {
         setUrl("https://maven.aliyun.com/repository/gradle-plugin")
         setUrl("https://maven.aliyun.com/repository/spring-plugin")
     }
+//    intellijPlatform {
+//        defaultRepositories()
+//    }
 }
 
 
@@ -28,7 +30,8 @@ dependencies {
     implementation("org.dom4j:dom4j:2.1.4")
     implementation("org.apache.xerces:xercesImpl:2.9.1")
     implementation("com.alibaba:fastjson:1.2.83_noneautotype")
-    implementation("com.taobao.util:taobao-express:3.1.8")
+//    implementation("com.taobao.util:taobao-express:3.1.9")
+    implementation("com.alibaba:QLExpress:3.3.3")
     implementation("com.google.guava:guava:30.1-jre")
     implementation("org.apache.commons:commons-collections4:4.4")
 //    使用本地的lib，避免按照插件的时候，还需要加载一会儿依赖
@@ -36,13 +39,32 @@ dependencies {
 
 //    用于发布到jetbrains仓库的时候签名
     // https://mvnrepository.com/artifact/org.jetbrains/marketplace-zip-signer
-//    compileOnly("org.jetbrains:marketplace-zip-signer:0.1.24" )
+    compileOnly("org.jetbrains:marketplace-zip-signer:0.1.24" )
+
+//    // 2.x   版本的的配置，移到了这里
+//    intellijPlatform {
+////        val type = providers.gradleProperty("platformType")
+////        val version = providers.gradleProperty("platformVersion")
+//
+//        intellijIdeaCommunity("2024.2.0.1")
+////        create("IC", "2024.2.0.2")
+//// 上面两行，是两种定义目标平台的方法
+//
+////        intellijIdeaUltimate("2024.2.0.1")
+//        bundledPlugin("com.intellij.java")
+//
+////        plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
+////        bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
+//        jetbrainsRuntime()
+//        javaCompiler()
+//    }
 }
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+// 1.x 版本的配置
 intellij {
-    version.set("2024.1.4")
+    version.set("2024.2.0.2")
 //    type.set("IC") // Target IDE Platform
     type.set("IU") // AI, CL, GW, GO, IC, IU, PS, PY, PC, RD
     plugins.set(listOf(/* Plugin Dependencies */))
@@ -55,44 +77,47 @@ idea{
     }
 }
 java {
+// 执行编译java的编译器版本
     sourceCompatibility = JavaVersion.VERSION_17
+
+//    编译的目标class 的版本，为了兼容更新的版本
+    // sourceCompatibility 应该小于等于 targetCompatibility
+    //targetCompatibility 不能比当前使用的gradle使用的java 版本大
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 
 tasks {
-//    // Set the JVM compatibility versions
-//    withType<JavaCompile> {
-//        sourceCompatibility = "17"
-//        targetCompatibility = "17"
-//    }
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
 
 //    版本号对照关系：https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html#platformVersions
     patchPluginXml {
 //        203 ，要求java 11
-//        222 ，要求java17
+//        222 ，要求java 17
+//        242 ，要求java 21
         sinceBuild.set("222")
-        untilBuild.set("242.*")
+//        untilBuild.set("242.*")
     }
 
     signPlugin {
         certificateChainFile.set(file("private/chain.crt"))
         privateKeyFile.set(file("private/private.pem"))
-//        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-        password.set("上面pem文件创建时候的密码")
+        password.set(System.getenv("INTELLIJ_PRIVATE_KEY_PASSWORD"))
+
     }
 
-//    发布到集团的插件仓库，参考：https://aliyuque.antfin.com/jinji.lf/note/nngd38?
-//    publishPlugin {
-//      token.set("idea")
-//        token.set("aone")
-//实际测试无效，有问题，传不上去
-//        正式仓库
-//        host.set("https://ide.alibaba-inc.com")
-//        测试的时候，发布到这个仓库
-//        host.set("http://ide.alibsaba.net")
-//    }
 
-    runIde { 
-        autoReloadPlugins = true
+// 发布到Jetbrasin到市场
+    publishPlugin {
+        token.set(System.getenv("ORG_GRADLE_PROJECT_intellijPublishToken"))
+        channels.set(listOf("beta"))
+//        alpha: https://plugins.jetbrains.com/plugins/alpha/list
+//        beta: https://plugins.jetbrains.com/plugins/beta/list
+//        eap: https://plugins.jetbrains.com/plugins/eap/list
+//        canary:   https://plugins.jetbrains.com/plugins/canary/list
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # WLKIdeaPlugin
 Idea插件项目，个人兴趣
+
+
+
+# 备注
+## gradle-jvm的版本对应关系
+来源于官网：
+https://docs.gradle.org/current/userguide/compatibility.html#compatibility
+
+
+![Java-gradle版本依赖关系](https://wanglikang-github-io.oss-cn-hangzhou.aliyuncs.com/images/20240904/d61cdebb94a442cda977a5b5489d9817.png?x-oss-process=image/auto-orient,1/interlace,1/quality,q_50/format,jpg)
+
+
+![Kotlin和gradle的版本关系](https://wanglikang-github-io.oss-cn-hangzhou.aliyuncs.com/images/20240904/ce7c28408ec94e1a8981c08f05aaa55b.png?x-oss-process=image/auto-orient,1/interlace,1/quality,q_50/format,jpg)
+![Kotlin和gradle的版本关系2](https://wanglikang-github-io.oss-cn-hangzhou.aliyuncs.com/images/20240904/f7ef641555df403f8e81d7f4e3d26b3a.png?x-oss-process=image/auto-orient,1/interlace,1/quality,q_50/format,jpg)
+
+
+## Java 字节码版本对应
+官网链接：https://javaalmanac.io/bytecode/versions/
+
+Class File Versions |  JDK Version | Bytecode Version
+Java 1.0	45.0
+Java 1.1	45.3
+Java 1.2	46.0
+Java 1.3	47.0
+Java 1.4	48.0
+Java 5	49.0
+Java 6	50.0
+Java 7	51.0
+Java 8	52.0
+Java 9	53.0
+Java 10	54.0
+Java 11	55.0
+Java 12	56.0
+Java 13	57.0
+Java 14	58.0
+Java 15	59.0
+Java 16	60.0
+Java 17	61.0
+Java 18	62.0
+Java 19	63.0
+Java 20	64.0
+Java 21	65.0
+Java 22	66.0
+Java 23	67.0
+Java 24	68.0
